b222c3afa0fdcd05cd7aa2728ce993157bfcfebd
ritesh537 <48156605+ritesh537@users.noreply.github.com>
2021-07-03 17:29:22 +0530

    Update testpipelines.groovy

--- jenkins/jobs/test/testpipelines.groovy
+++ jenkins/jobs/test/testpipelines.groovy
@@ -2,7 +2,7 @@ pipelineJob("gitcheckout"){
     description('copy-git-repo')
     parameters{
         choiceParam('gitUrl',['https://github.com/ritesh537/ml-project'], 'Git Repo')
-        choiceParam('gitBranch', ['main', 'feature'], 'Branch')
+        choiceParam('gitBranch', ['master', 'feature'], 'Branch')
 
     }
     definition{
