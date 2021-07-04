ac0fda11aa292b46330dcea203abbf60a72858f5
ritesh537 <48156605+ritesh537@users.noreply.github.com>
2021-07-04 16:33:02 +0530

    Update checkoutgit.groovy

--- jenkins/pipelines/checkoutgit.groovy
+++ jenkins/pipelines/checkoutgit.groovy
@@ -5,7 +5,24 @@ stage('copy-git-repo'){
         $class: 'GitSCM',
         branches:[[name: gitBranch]],
         doGenerateSubmoduleConfigurations:false,
+        extensions: [[$class: 'RelativeTargetDirectory', relativeTargetDir: 'ml-project']],
         userRemoteConfigs:[[url: gitUrl]]
     ])
+}
+stage('uplod to artifactory'){
+    sh ("tar -cvzf ml-project.tar.gz ml-project")
+    def buildrepo = Artifactory.newServer url : 'http://13.52.79.180:8082/artifactory'
+    buildrepo.credentialsId = 'artifactory'
+   
+    def uploadSpec = """{
+    "files": [
+    {
+      "pattern" : "ml-project.tar.gz",
+      "target" :"rks/"
+     
+    }
+    ]
+    }"""
+     buildrepo.upload(uploadSpec)
 }
 }
